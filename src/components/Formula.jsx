e520fde5b0b5aa1481957df9ff0208e7c9194b87
Haley <haleykbanfield@gmail.com>
2025-12-25 00:17:19 +0900

    rejiggered code and learned about useState. There's a test reset button in Table.jsx currently

--- src/components/Formula.jsx
+++ src/components/Formula.jsx
@@ -1,11 +1,7 @@
-import materials from './material_data.js';
 import MaterialRow from './MaterialRow.jsx';
 
-
-// useState?
-
-const Formula = () => {
-    return materials.map((material, index) => {
+const Formula = ({data}) => {
+    return data.map((material, index) => {
         return <MaterialRow key={index} material={material} />       
     })
 }
