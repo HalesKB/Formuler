27a8bde379f33a89a3fcb6cb796e271a757ac960
Haley <haleykbanfield@gmail.com>
2026-01-11 23:37:46 +0900

    Fiddling with code trying to get it to work with nothing to show for it.

--- src/components/Formula.jsx
+++ src/components/Formula.jsx
@@ -1,8 +1,20 @@
+// I believe I am abandoning this component as it would be easier to implement
+// SortableContext directly on the Table page by rendering the material rows there. 
+// I'm keeping this around as a vestigial member of the project.
+
 import MaterialRow from './MaterialRow.jsx';
+import {
+    SortableContext,
+    verticalListSortingStrategy
+} from "@dnd-kit/sortable";
 
 const Formula = ({data}) => {
     return data.map((material, index) => {
-        return <MaterialRow key={index} material={material} />       
+        return (
+        <SortableContext items={materials} strategy={verticalListSortingStrategy}>
+            <MaterialRow key={index} material={material} />
+        </SortableContext>  
+        )
     })
 }
 
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,29 +1,42 @@
-import { useState } from "react";
-import {DndContext} from "@dnd-kit/core";
-import Formula from "./Formula.jsx";
 import material_data_array from "./material_data.js";
+import MaterialRow from './MaterialRow.jsx';
+import { useState } from "react";
+import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+    SortableContext,
+    verticalListSortingStrategy,
+    arrayMove
+} from "@dnd-kit/sortable";
 
 const Table = () => {
     const [materials, setMaterials] = useState(material_data_array)
+
     return (
         <>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Material</th>
-                        <th>Dilution</th>
-                        <th>Grams</th>
-                        <th>Percent in Concentrate</th>
-                        <th>Percent of Total</th>
-                        <th>Note</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <Formula data={materials} />
-                </tbody>
-            </table>
+            <DndContext collisionDetection={closestCenter}>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Material</th>
+                            <th>Dilution</th>
+                            <th>Grams</th>
+                            <th>Percent in Concentrate</th>
+                            <th>Percent of Total</th>
+                            <th>Note</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <SortableContext items={materials} strategy={verticalListSortingStrategy}>
+                            {materials.map((material) => (
+                            <MaterialRow key={material.id} material={material} />
+                            ))}
+                        </SortableContext>
+                    </tbody>
+                </table>
+            </DndContext>
             <button className="btn" onClick={() => setMaterials([])}>Reset</button>
         </>
     )
 }
+
 export default Table
